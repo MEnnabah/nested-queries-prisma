b43fc01931fa864032c1d3018216badae34b3b27
MEnnabah <m@ennabah.com>
2023-04-17 19:23:51 +0400

    seed db

--- prisma/migrations/20230417150121_init/migration.sql
+++ prisma/migrations/20230417150121_init/migration.sql
@@ -22,3 +22,7 @@ ALTER TABLE "Properties" ADD CONSTRAINT "Properties_catId_fkey" FOREIGN KEY ("ca
 
 -- AddForeignKey
 ALTER TABLE "Cat" ADD CONSTRAINT "Cat_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Cat"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- Seed
+INSERT INTO "Cat" ("id", "name", "age", "parentId") VALUES (1, 'Grumpy Cat', 6, null), (2, 'Garfield', 3, 1), (3, 'Keyboard Cat', 2, 1), (4, 'Nyan Cat', 1, 1);
+INSERT INTO "Properties" ("id", "color", "catId") VALUES (1, 'orange', 1), (2, 'gray', 2), (3, 'rainbow', 3);
