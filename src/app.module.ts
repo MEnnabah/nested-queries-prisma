9abc6225b34df5126180a1720c7c43d43422030a
MEnnabah <m@ennabah.com>
2023-04-17 23:57:41 +0400

    another example with mercurius

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
+    GraphQLModule.forRoot<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
+      graphiql: true,
       autoSchemaFile: 'schema.gql',
-      installSubscriptionHandlers: true,
-      buildSchemaOptions: {
-        numberScalarMode: 'integer',
-      },
+      // buildSchemaOptions: {
+      //   numberScalarMode: 'integer',
+      // },
     }),
     CatsModule,
   ],
